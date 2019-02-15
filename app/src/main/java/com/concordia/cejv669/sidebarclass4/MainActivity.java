5296e4495788b7e0bdbf7653f5a4ac577f04eedb
Yushu <yushuhuang90@126.com>
2019-02-14 19:55:10 -0500

    Initial commit

---
+++ app/src/main/java/com/concordia/cejv669/sidebarclass4/MainActivity.java
@@ -0,0 +1,43 @@
+package com.concordia.cejv669.sidebarclass4;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dl = (DrawerLayout)findViewById(R.id.drawer_layout);
+        t = new ActionBarDrawerToggle(this, dl,R.string.nav_open, R.string.nav_close);
+        dl.addDrawerListener(t);
+        t.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(t.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
+       if (item.getItemId()==R.id.nav_gallery)
+       {
+           Toast.makeText(getApplicationContext(),"Gallery",Toast.LENGTH_LONG).show();
+           System.out.print("test");
+       }
+        return true;
+    }
+}
